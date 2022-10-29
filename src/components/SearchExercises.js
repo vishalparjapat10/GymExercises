0af6f6a921430e052e21cec96523d71ba52dfcc6
vishalparjapat10 <vishalparjapat12527@gmail.com>
2022-10-29 13:31:59 +0530

    :construction: exercisedetail pages started

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -9,7 +9,7 @@ const SearchExercises = ({ setExercises,bodyPart,setBodyPart }) => {
 
   useEffect (() =>{
     const fetchExercisesData = async () =>{
-      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',exerciseOptions);
+      // const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList',exerciseOptions);
       console.log(bodyPartsData);
       setBodyParts(['all',...bodyPartsData]);
     }
--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React,{ useEffect,useState} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions,fetchData, youtubeOptions } from '../utils/fetchData';
+import SimilarExercises from '../components/SimilarExercises';
+import ExerciseVideos from '../components/ExerciseVideos';
+import Details from '../components/Details';
 
 const ExerciseDetail = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const { id } = useParams();
+
+  useEffect(() =>{
+    const fetchExercisesData = async () =>{
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
+
+      const exercisesDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,exerciseOptions);
+      setExerciseDetail(exercisesDetailData);
+
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=${exerciseDetail.name}`,youtubeOptions);
+    }
+
+    fetchExercisesData();
+  },[id]);
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Details exerciseDetail={exerciseDetail}/>
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
